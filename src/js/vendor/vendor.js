78c61ad909b24a77dfe32652934eacfa9a7032b2
xeonalex <lux1995@yandex.ru>
2016-12-16 00:32:35 +0200

    tel mask added

--- src/js/vendor/vendor.js
+++ src/js/vendor/vendor.js
@@ -1,3 +1,4 @@
 console.log('hello from vendor.js');
 //="../../../bower_components/jquery/dist/jquery.js"
+//="../../../bower_components/jquery.maskedinput/dist/jquery.maskedinput.js"
 //"../../../bower_components/bPopup/jquery.bpopup.js"
