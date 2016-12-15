c9e540b10773c6875508f5102b878bcc4a81185a
xeonalex <lux1995@yandex.ru>
2016-12-16 00:19:14 +0200

    about block added

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
 console.log('hello from script.js');
+
+
+// Блок "Лечебно-диагностический центр"
+//
+
+$('.about_more-link').click(function(){
+	$('.about_content').toggleClass('about_full');
+})
--- src/js/vendor/vendor.js
+++ src/js/vendor/vendor.js
@@ -1,3 +1,3 @@
 console.log('hello from vendor.js');
 //="../../../bower_components/jquery/dist/jquery.js"
-//="../../../bower_components/bPopup/jquery.bpopup.js"
+//"../../../bower_components/bPopup/jquery.bpopup.js"
